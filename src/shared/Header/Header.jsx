9865c2e992caae2dce2dfb4fe5afcf231bb7436c
alamin360am <alaminhimu0@gmail.com>
2023-05-20 14:17:44 +0600

    all basic setup done

---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <div className="nav-logo"></div>
+                <div className="nav-link"></div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
