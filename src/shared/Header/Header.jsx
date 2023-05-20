1809daabad5be7ce3358728135ce427dd01d47a1
alamin360am <alaminhimu0@gmail.com>
2023-05-20 14:52:54 +0600

    header complete

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,11 +1,27 @@
 import './Header.css'
+import logo from './../../../public/logo.png'
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <div className="nav-logo"></div>
-                <div className="nav-link"></div>
+                <div className="nav-logo">
+                    <img src={logo} alt="" />
+                    <span>Toy House</span>
+                </div>
+                <div className="nav-link">
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/all-toys'>All Toys</NavLink>
+                    <NavLink to='/my-toys'>My Toys</NavLink>
+                    <NavLink to='/add-a-toy'>Add A Toy</NavLink>
+                    <NavLink to='/blog'>Blogs</NavLink>
+                </div>
+                <div className="user-box">
+                    <div className="user-img"></div>
+                    <Link to='/login' className='btn'>Log In</Link>
+                    <Link to='/logout' className='btn'>Log out</Link>
+                </div>
             </nav>
         </header>
     );
